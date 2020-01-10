10556595c4cd0f9998b1bc29421aadf6ec59bc23
Stanislav <liesnychevskyi.s@gmail.com>
2020-01-10 13:51:37 +0200

    triangle typing

--- practice/download_file_from_remote.py
+++ practice/download_file_from_remote.py
@@ -12,15 +12,34 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 print("Please wait, connecting with remoter server...")
 ssh_client.connect(hostname=ip, username=user_name, password=passwd)
-#  cmd = "cd folderForFile"
-cmd_1 = "find . -name file.docx"
-cmd_2 = "cd .."
-cmd_3 = "ls"
-print("Please wait, executing command on remoter server...")
-stdin, stdout, stderr = ssh_client.exec_command(cmd_3)
 
-stdout = stdout.readlines()
-print(stdout)
-print("Successfully executed command on remoter !!!")
-
+sftp_client = ssh_client.open_sftp()
+print(dir(sftp_client))
+# sftp_client.get('path from remote machine', 'file_name_to_storing')
+sftp_client.chdir("/mydir")
+print(sftp_client.getcwd())
+sftp_client.close()
 ssh_client.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cmd = "cd folderForFile"
+# cmd_1 = "find . -name file.docx"
+#
+# print("Please wait, executing command on remoter server...")
+# stdin, stdout, stderr = ssh_client.exec_command(cmd_1)
+#
+# stdout = stdout.readlines()
+# print(stdout)
+# print("Successfully executed command on remoter !!!")
+
